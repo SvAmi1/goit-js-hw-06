7d45485aa26795b856c33c55860643b67038fa98
SvAmi1 <svmoshko877@gmail.com>
2023-05-29 16:45:04 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const itemEl = document.createElement('li');
+
+const markup = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
+
+const listEl = document.querySelector("#ingredients");
+listEl.append(markup);
+
+
+
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+// const listOfIngredients = document.querySelector('#ingredients');
+
+// const makeIngredientsItems = elements => {
+//   return elements.map(element => {
+//     const itemElements = document.createElement('li');
+//     itemElements.textContent = element;
+//     itemElements.classList.add('item');
+//     return itemElements;
+//   });
+// };
+
+// const itemIngrediens = makeIngredientsItems(ingredients);
+// listOfIngredients.append(...itemIngrediens);
