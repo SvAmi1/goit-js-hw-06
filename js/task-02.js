dea3b2017102e8ce892a6887162b243bff3bc47a
SvAmi1 <svmoshko877@gmail.com>
2023-05-30 12:01:02 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,34 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const itemEl = document.createElement('li');
 
-const markup = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
+const makeIngredientsItems = (ingredients) => { 
+  return ingredients.map (ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+  return itemEl;
+}); 
+};
+
+const elements = makeIngredientsItems(ingredients);
 
 const listEl = document.querySelector("#ingredients");
-listEl.append(markup);
 
-
-
-// const ingredients = [
-//   'Potatoes',
-//   'Mushrooms',
-//   'Garlic',
-//   'Tomatos',
-//   'Herbs',
-//   'Condiments',
-// ];
-
-// const listOfIngredients = document.querySelector('#ingredients');
-
-// const makeIngredientsItems = elements => {
-//   return elements.map(element => {
-//     const itemElements = document.createElement('li');
-//     itemElements.textContent = element;
-//     itemElements.classList.add('item');
-//     return itemElements;
-//   });
-// };
-
-// const itemIngrediens = makeIngredientsItems(ingredients);
-// listOfIngredients.append(...itemIngrediens);
+listEl.append(...elements);
