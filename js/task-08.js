0fe37b50f5432cc97c67b4917ed27e251039090e
SvAmi1 <svmoshko877@gmail.com>
2023-06-05 14:36:54 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit (e) {
+    e.preventDefault();
+    const { 
+        elements: {email, password}, } = e.currentTarget;
+
+    if (email.value == "" || password.value == "") {
+        alert("All form fields must be filled in");
+        return;
+    } 
+    else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(formData);
+        formEl.reset();
+    }
+  };
