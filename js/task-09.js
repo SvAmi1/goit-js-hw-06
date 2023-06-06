b963e97bbabab4bf5f40c8ac39c901507ac961f0
SvAmi1 <svmoshko877@gmail.com>
2023-06-06 10:11:48 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+
+btnEl.addEventListener("click", handleBtnChangeColor);
+
+function handleBtnChangeColor() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  textEl.textContent = color;
+}
