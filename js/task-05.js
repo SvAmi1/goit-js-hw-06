3fdf3df8d868c8625fa1eb68624795fe15fad9e1
SvAmi1 <svmoshko877@gmail.com>
2023-06-07 10:25:54 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,8 @@ const greetingEl = document.querySelector('#name-output');
 
 formInputEl.addEventListener('input', (event) => {
     
-    if (event.target.value.trim() == "") {
-        greetingEl.textContent == 'Anonymous';
+    if (event.target.value.trim() === "") {
+        greetingEl.textContent = 'Anonymous';
         return;
     };
     greetingEl.textContent = event.target.value;
